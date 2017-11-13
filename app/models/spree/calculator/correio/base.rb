b9c772dfe94adc5acacd1300308cd81369f30659
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2017-11-13 10:41:47 -0200

    Use origin zipcode from package’s stock_location

--- app/models/spree/calculator/correio/base.rb
+++ app/models/spree/calculator/correio/base.rb
@@ -1,7 +1,6 @@
 module Spree
   class Calculator::Correio::Base < Spree::ShippingCalculator
-    preference :zipcode, :string
-    preference :default_weight, :decimal, :default => 0.0
+    preference :default_weight, :decimal, :default => 0.1
     preference :default_box_weight, :decimal, :default => 0.25
     preference :default_box_price, :decimal, :default => 0.0
     preference :box_x, :integer, :default => 36
@@ -10,44 +9,41 @@ module Spree
     preference :company_code, :string
     preference :password, :string
 
-    def cached_response(order)
-      # response = Rails.cache.fetch(cache_key(order)) do
-        response = correio_info(order)
-      # end
-      response
-    end
+    def compute_package(package)
+      data = cached_info(package).
+        select{ |d| services.include?(d) }.
+       reject{ |k,v| v.error? }.
+       values.
+       min_by(&:valor)
 
-    def compute(object)
-      begin
-        order = find_order(object)
-        response_for_service = cached_response(order).select{ |d| services.include?(d) }.reject{ |k,v| v.error? }
-                               .values.min_by(&:valor)
-        response_for_service.valor + preferred_default_box_price
-      rescue
-        nil
-      end
-    end
-
-    def timing(object)
-      begin
-        order = find_order(object)
-        response_for_service = cached_response(order).select{ |d| services.include?(d) }.reject{ |k,v| v.error? }
-                                 .values.min_by(&:valor)
-        response_for_service.prazo_entrega
-      rescue
-        nil
-      end
+      data.valor + preferred_default_box_price
+    rescue
+      nil
     end
 
     private
-    def correio_info(order)
-      total_weight = order_total_weight(order)
-      return {} if total_weight == 0
+
+    def cached_info(package)
+      Rails.cache.fetch(cache_key(package), compress: true, expires_in: 12.hours) do
+        correio_info(package)
+      end
+    end
+
+    def cache_key(package)
+      order = package.order
+      zipcode = order.ship_address.zipcode
+      stock_zipcode = package.stock_location.zipcode
+      line_items_hash = Digest::MD5.hexdigest(package.contents.map { |i| "#{i.variant.id}_#{i.quantity}" }.join("|"))
+      @cache_key = "correio-#{order.number}-#{zipcode}-#{line_items_hash}-#{stock_zipcode}".gsub(" ","")
+    end
+
+    def correio_info(package)
+      weight = package.weight
 
       request_attributes = {
-        :cep_origem => preferred_zipcode,
-        :cep_destino => order.ship_address.zipcode.to_s,
-        :peso => total_weight,
+        :cep_origem => package.stock_location.zipcode,
+        :cep_destino => package.order.ship_address.zipcode.to_s,
+        :peso => weight.zero? ?  preferred_default_weight : weight,
         :comprimento => preferred_box_x,
         :largura => preferred_box_y,
         :altura => preferred_box_z,
@@ -70,34 +66,6 @@ module Spree
       response
     end
 
-    def order_total_weight(order)
-      weight = preferred_default_box_weight || 0
-      order.line_items.each do |item|
-        weight += item.quantity * (item.variant.weight || 0)
-      end
-      weight == 0 ? preferred_default_weight : weight
-    end
-
-    def find_order(object)
-      if object.is_a?(Array)
-        order = object.first.order
-      elsif object.is_a?(Shipment)
-        order = object.order
-      elsif object.is_a?(Spree::Stock::Package)
-        order = object.order
-      else
-        order = object
-      end
-      order
-    end
-
-    def cache_key(order)
-      addr = order.ship_address
-      line_items_hash = Digest::MD5.hexdigest(order.line_items.map {|li| li.variant_id.to_s + "_" + li.quantity.to_s }.join("|"))
-      credentials = "#{preferred_company_code}-#{preferred_password}"
-      @cache_key = "correio-#{order.number}-#{addr.country.iso}-#{addr.state ? addr.state.abbr : addr.state_name}-#{addr.city}-#{addr.zipcode}-#{line_items_hash}-#{credentials}-#{preferred_zipcode}".gsub(" ","")
-    end
-
     def available_services
       Spree::Calculator.where(type: available_calculators_class_name).map(&:services).flatten
     end
@@ -105,6 +73,5 @@ module Spree
     def available_calculators_class_name
       Spree::Calculator::Correio::Scaffold.descendants.map(&:name)
     end
-
   end
 end
--- app/models/spree/calculator/correio/pac.rb
+++ app/models/spree/calculator/correio/pac.rb
@@ -1,6 +1,6 @@
 class Spree::Calculator::Correio::Pac < Spree::Calculator::Correio::Scaffold
 
   def services
-    [:pac_com_contrato, :pac]
+    [:pac]
   end
 end
