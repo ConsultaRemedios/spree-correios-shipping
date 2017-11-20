660507b685d186b2f1fae9814e36804dcdb19501
Eduardo Resende <ed.resende@gmail.com>
2017-11-20 11:35:20 -0200

    Remove e_sedex calculator
    E-Sedex plans were discontinued by Correios

--- app/models/spree/calculator/correio/e_sedex.rb
+++
@@ -1,6 +0,0 @@
-class Spree::Calculator::Correio::ESedex < Spree::Calculator::Correio::Scaffold
-
-  def services
-    [:e_sedex, :e_sedex_prioritario, :e_sedex_express, :e_sedex_grupo_1, :e_sedex_grupo_2, :e_sedex_grupo_3]
-  end
-end
